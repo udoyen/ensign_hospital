97b342682141f3bd9d3ca41c2f9f316e8a474a13
george udosen <datameshprojects@gmail.com>
2016-09-10 10:58:25 +0100

    Modal form add.
    Modal form template added.

--- assets/js/jumbo-height.js
+++ assets/js/jumbo-height.js
@@ -1,10 +1,14 @@
+/**
+ * Created by george on 9/9/16.
+ */
+
 $(document).ready(function(){
     // makes the jumbo element same height as wviewport
     function windowH() {
         var wH = $(window).height();
-    
-        $('#header-jumbo').css({height: wH});
+
+        $('#home').css({height: wH});
     }
     windowH();
-    
+
 });
